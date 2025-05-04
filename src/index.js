e2330f82e0a45b6ea4d2c4f55b65a3732f63a7a1
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-05-04 09:48:01 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// Create React 18 root and render App
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
