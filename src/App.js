f1cff498b27b111c27410e206830bd90f5943172
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-05-09 09:47:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FollowUps from './components/FollowUps';
 import FollowUpForm from './components/FollowUpForm';
 import TodayFollowUps from './components/TodayFollowUps';
 import EditCaseForm from './components/EditCaseForm';
+import FaceUpload from './components/FaceUpload'; // 👈 Import chesam
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/followups/new/:caseId" element={<FollowUpForm />} />
           <Route path="/followups/today" element={<TodayFollowUps />} />
           <Route path="/cases/edit/:caseId" element={<EditCaseForm />} />
+          <Route path="/face-upload" element={<FaceUpload />} /> {/* 👈 New Route added */}
         </Routes>
       </div>
     </Router>
