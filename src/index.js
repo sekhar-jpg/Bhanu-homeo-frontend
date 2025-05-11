64163d8a6e058bf3054566c12a02827377d9b8be
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-05-11 17:52:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-// src/index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Create React 18 root and render App
+// Create React 18 root and render App without StrictMode
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
