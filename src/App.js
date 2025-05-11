3214ad7dd9b8718c9fbe7343aedfa61d260c7329
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-05-12 00:20:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FollowUps from './components/FollowUps';
 import FollowUpForm from './components/FollowUpForm';
 import TodayFollowUps from './components/TodayFollowUps';
 import EditCaseForm from './components/EditCaseForm';
+import CaseSheetForm from './components/CaseSheetForm'; // ✅ newly added form
 
 function App() {
   return (
@@ -13,10 +14,18 @@ function App() {
       <div>
         <Navbar />
         <Routes>
+          {/* Homepage shows the AddCase screen */}
           <Route path="/" element={<AddCase />} />
+          
+          {/* Route to see full Case Sheet Form */}
+          <Route path="/new-case" element={<CaseSheetForm />} />
+
+          {/* Follow-up related routes */}
           <Route path="/followups" element={<FollowUps />} />
           <Route path="/followups/new/:caseId" element={<FollowUpForm />} />
           <Route path="/followups/today" element={<TodayFollowUps />} />
+
+          {/* Edit case screen */}
           <Route path="/cases/edit/:caseId" element={<EditCaseForm />} />
         </Routes>
       </div>
