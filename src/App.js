6866512f849ac6400d6cbcc19519505d5312b94a
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-05-12 11:58:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
-import './App.css'; // For styling bottom tab (we'll create this next)
-import CaseSheetForm from './components/CaseSheetForm';
-import CasesList from './components/CasesList'; // Real or dummy case list screen
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import CaseSheetForm from "./components/CaseSheetForm";
+import FollowUpForm from "./components/FollowUpForm";
+import CasesList from "./components/CasesList";
+import "./App.css"; // This should style your bottom tabs
 
-// Create Bottom Tabs (For Web)
+// Bottom tab component
 const BottomTabs = () => {
   return (
     <div className="bottom-tabs">
-      <NavLink to="/" className="tab-link">
+      <NavLink to="/" className="tab-link" end>
         📝 New Case
+      </NavLink>
+      <NavLink to="/followup" className="tab-link">
+        🔁 Follow Up
       </NavLink>
       <NavLink to="/cases" className="tab-link">
         📋 Cases
@@ -18,15 +22,17 @@ const BottomTabs = () => {
   );
 };
 
+// Main app component
 const App = () => {
   return (
     <Router>
       <div className="content">
         <Routes>
           <Route path="/" element={<CaseSheetForm />} />
+          <Route path="/followup" element={<FollowUpForm />} />
           <Route path="/cases" element={<CasesList />} />
         </Routes>
-        <BottomTabs /> {/* Add the bottom tabs navigation */}
+        <BottomTabs />
       </div>
     </Router>
   );
