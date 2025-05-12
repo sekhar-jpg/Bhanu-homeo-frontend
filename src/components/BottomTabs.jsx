5bd4620d0f0bfe943927b65be13e76685f1e8ed8
sekhar-jpg <bhanuhomeohospital@gmail.com>
2025-05-12 09:05:00 +0530

    Update BottomTabs.jsx

--- src/components/BottomTabs.jsx
+++ src/components/BottomTabs.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './BottomTabs.css'; // We'll create this next
+import './BottomTabs.css';
+import { FaPlus, FaListAlt, FaSync } from 'react-icons/fa'; // Example icons
 
 const BottomTabs = () => {
   return (
     <div className="bottom-tabs">
       <NavLink to="/" className="tab-link">
-        📝 New Case
+        <span aria-hidden="true"><FaPlus /></span>
+        New Case
       </NavLink>
       <NavLink to="/cases" className="tab-link">
-        📋 Cases
+        <span aria-hidden="true"><FaListAlt /></span>
+        Cases
       </NavLink>
       <NavLink to="/followups" className="tab-link">
-        🔁 Follow-ups
+        <span aria-hidden="true"><FaSync /></span>
+        Follow-ups
       </NavLink>
     </div>
   );
